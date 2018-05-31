e48d9301a4aa4fef2088877acb08a898db176b0b
hallobabo <artifexx@hotmail.de>
2018-05-31 20:24:00 +0200

    ff

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+	public float threshold;
+
+	void  FixedUpdate () {
+		if (transform.position.y < threshold)
+			transform.position = new Vector3(0, 0, 0);
+	}
+
+}
